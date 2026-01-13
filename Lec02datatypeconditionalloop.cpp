5e75954410fc285884940d6f2724a3eebfcdabf3
nidhisinghnidhiclg-lgtm <nidhisinghnidhiclg@gmail.com>
2026-01-13 13:59:34 +0530

    Add files via upload
    Day 1: Logic Building - Flowcharts and Basic Conditional Statements

---
+++ Lec02datatypeconditionalloop.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+/*int a ;
+cout<<"entre a number";
+cin>>a;
+if(a>0){ 
+cout<<"a is positive ";
+}
+else{  
+cout<<" a is negative" ;*/
+int a ;
+cout<<"entre a number   ";
+cin>>a;
+if(a>0) {
+cout<<"a is positive ";}
+else if( a<0){
+  cout<<"a is negative number"  ;
+
+}
+else {
+
+   cout<<"a is zero number" ;
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
